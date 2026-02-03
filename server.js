f852d20f6464f22e74b52cfee8bda4c291761100
Anuj Gupta <anuj@inquisitivedigital.com>
2026-02-03 17:22:24 +0530

    fixed

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://godrej-chi.vercel.app',  
+    'https://godrej-chi.vercel.app',
+    'https://godrej-chi.vercel.app/',
     'https://godrejarden.in',
     'https://www.godrejarden.in'
 ];
